d0f3e4159ea0c03872144e259af1a4fef3e573d4
Tami Personal <17oshinnaike@gmail.com>
2026-02-18 18:03:57 +0000

    made some adjustments to script

--- CW1_eval_script.py
+++ CW1_eval_script.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 # Import training data
 trn = pd.read_csv('CW1_train.csv')
-X_tst = pd.read_csv('CW1_test.csv') # This does not include true outcomes (obviously)
+tst = pd.read_csv('CW1_test.csv') # This does not include true outcomes (obviously)
 logging.info("Training and test data loaded")
 
 # Dropping 0 values for x, y, z 
@@ -41,11 +41,12 @@ for col in categorical_cols:
 logging.info("Categorial variable encoding finished")
 
 X, y = trn_copy[['depth', 'table', 'a1', 'a4', 'b1', 'b3']].values, trn_copy['outcome'].values
-X_trn, X_tst, y_trn, y_tst = train_test_split(X, y, test_size=0.3, random_state=0)
+X_tst = tst[['depth', 'table', 'a1', 'a4', 'b1', 'b3']].values
+
 
 pipeline = Pipeline(steps=[('scaler', StandardScaler()),
                            ('regressor', GradientBoostingRegressor())])
-model = pipeline.fit(X_trn, (y_trn))
+model = pipeline.fit(X, (y))
 logging.info("Inital training finished")
 
 alg = GradientBoostingRegressor()
@@ -59,35 +60,20 @@ hyperparameters = {
 
 score = make_scorer(r2_score)
 gridsearch = GridSearchCV(alg, hyperparameters, scoring=score, cv=3, return_train_score=True)
-gridsearch.fit(X_trn, y_trn)
+gridsearch.fit(X, y)
 
 model = gridsearch.best_estimator_
 logging.info("Training finished")
 
 # Test set predictions
-logging.info("Predicting outcomes with test data...")
-yhat_lm = model.predict(X_tst)
+logging.info("Predicting outcomes with test data set...")
+predictions = model.predict(X_tst)
 
 # Format submission:
 # This is a single-column CSV with nothing but your predictions
-out = pd.DataFrame({'yhat': yhat_lm})
+out = pd.DataFrame({'predictions': predictions})
 out.to_csv('CW1_submission_K23171764.csv', index=False) 
 
-################################################################################
-
-# At test time, we will use the true outcomes
-#tst = pd.read_csv('CW1_test_with_true_outcome.csv') # You do not have access to this
-
-# This is the R^2 function
-def r2_fn(yhat):
-    eps = y_tst - yhat
-    rss = np.sum(eps ** 2)
-    tss = np.sum((y_tst - y_tst.mean()) ** 2)
-    r2 = 1 - (rss / tss)
-    return r2
-
-# How does the linear model do?
-print(r2_fn(yhat_lm))
 
 
 
